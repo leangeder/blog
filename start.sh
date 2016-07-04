2f7aa1b4c66ef6025e7d61ee4c868c6944260a0c
Gregory BULGARE <gregoryb@qinec.com>
2016-07-04 20:02:08 +0100

    new features

--- start.sh
+++ start.sh
@@ -1,38 +1,32 @@
 #!/usr/bin/env sh
 
-DM_PATH=$(which docker-machine)
-DC_PATH=$(which docker-compose)
-HOME_BIN_PATH=$HOME/bin
-DM_VE="virtualbox"
+BIN_PATH=/usr/local/bin
+DM_PATH="$BIN_PATH/docker-machine"
+DC_PATH="$BIN_PATH/docker-compose"
 DM_VERSION="v0.7.0"
 DC_VERSION="1.6.2"
-DM_SHARED_PATH=$PWD
-DM_NAME="dev"
-DM_HOSTNAME="$(basename $DM_SHARED_PATH).local"
-DC_FILE="$PWD/docker-compose.yml"
+HOST_FILE="/etc/hosts"
+DM_VE="virtualbox"                                                              
 
 get_dm_tools () {
-	mkdir -p $HOME_BIN_PATH
-	if [ "X$DM_PATH" = "X" ]; then
-		printf "%s" "Docker Machine is missing, it will be download and setting on $HOME_BIN_PATH..."
-		sudo curl -L https://github.com/docker/machine/releases/download/${DM_VERSION}/docker-machine-`uname -s`-`uname -m` 2>/dev/null > $HOME_BIN_PATH/docker-machine
-		sudo chmod +x $HOME_BIN_PATH/docker-machine
+	mkdir -p $BIN_PATH
+	if [ "X$(which docker-machine)" = "X" ]; then
+		printf "%s" "Docker Machine is missing, it will be download and setting on $BIN_PATH..."
+		sudo -- sh -c -e "curl -L https://github.com/docker/machine/releases/download/${DM_VERSION}/docker-machine-`uname -s`-`uname -m` 2>/dev/null > $DM_PATH";
+		sudo chmod +x $DM_PATH
 		printf "%s\n" "Done"
 	fi
 
-	if [ "X$DC_PATH" = "X" ]; then
-		printf "%s" "Docker Compose is missing, it will be download and setting on $HOME_BIN_PATH..."
-        sudo curl -L https://github.com/docker/compose/releases/download/${DC_VERSION}/docker-compose-`uname -s`-`uname -m` 2>/dev/null > $HOME_BIN_PATH/docker-compose
-        sudo chmod +x $HOME_BIN_PATH/docker-compose
+	if [ "X$(which docker-compose)" = "X" ]; then
+		printf "%s" "Docker Compose is missing, it will be download and setting on $BIN_PATH..."
+        sudo -- sh -c -e "curl -L https://github.com/docker/compose/releases/download/${DC_VERSION}/docker-compose-`uname -s`-`uname -m` 2>/dev/null > $DC_PATH";
+        sudo chmod +x $DC_PATH
 		printf "%s\n" "Done"
     fi
-	export PATH=$PATH:$(echo $HOME_BIN_PATH)
-	DM_PATH=$(which docker-machine)
-	DC_PATH=$(which docker-compose)
 }
 
 start_dm_ve () {
-	if [ "X$(docker-machine inspect $DM_NAME 2>&1)" = "XHost does not exist: \"$DM_NAME\"" ]; then
+	if [ "X$($DM_PATH inspect $DM_NAME 2>&1)" = "XHost does not exist: \"$DM_NAME\"" ]; then
 		printf "%s" "Docker Machine $DM_NAME did not exist. Creation of the docker machine..."
 		$DM_PATH create -d $DM_VE $DM_NAME > /dev/null 2>&1
 		printf "%s\n" "Done"
@@ -52,6 +46,7 @@ shared_dm_ve () {
         TMP_SHARED_PATH=""
 		TMP_SHARED_MNT_PATH=""
 		VBOX_MANAGE="C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage"
+		HOST_FILE=""
     fi                                                                          
     $DM_PATH stop $DM_NAME > /dev/null 2>&1
     $VBOX_MANAGE sharedfolder remove $DM_NAME --name $TMP_SHARED_NAME > /dev/null 2>&1
@@ -73,40 +68,85 @@ shared_dm_ve () {
 start_dc () {
 
 	if [ "X$DC_FILE" != "X" ] && [ -f $DC_FILE  ]; then
-		eval $($HOME_BIN_PATH/docker-machine env $DM_NAME )
+		eval $($DM_PATH env $DM_NAME )
 		printf "%s" "Execution of docker compose file $DC_FILE..."
 		$DC_PATH -f $DC_FILE up --force-recreate -d > /dev/null 2>&1
+		printf "%s\n" "Done"
 	fi
-	printf "%s\n" "Done"
 }
 
 set_hosts() {
-    if [ ! -n "$(grep $DM_HOSTNAME /etc/hosts)" ]; then
-		sudo -- sh -c -e "echo '$(docker-machine ip ${DM_NAME})\t$DM_HOSTNAME' >> /etc/hosts";
+    if [ ! -n "$(grep $DM_HOSTNAME $HOST_FILE)" ]; then
+		sudo -- sh -c -e "echo '$($DM_PATH ip ${DM_NAME})\t$DM_HOSTNAME' >> $HOST_FILE";
 	fi
-	if [ -n "$(grep $DM_HOSTNAME /etc/hosts)" ]; then
-		printf "%s\n" "The IP address is $(docker-machine ip ${DM_NAME}). It is  accessible with hostname $DM_HOSTNAME";
+	if [ -n "$(grep $DM_HOSTNAME $HOST_FILE)" ]; then
+		printf "%s\n" "The IP address is $($DM_PATH ip ${DM_NAME}). It is  accessible with hostname $DM_HOSTNAME";
     fi
 }
 
-#for args in "$@"
-#do
-#case $i in
-#    -e=*|--environnementenvironnement=*)
-#    DM_VE="${i#*=}"
-#    shift # past argument=value
-#    ;;
-#	-s=*|--shared_path=*)
-#	DM_SHARED_PATH="${i#*=}"
-#	shift
-#	;;
-#    *)
-#	DM_VE="virtualbox"
-#	DM_SHARED_PATH=
-#            # unknown option
-#    ;;
-#esac
-#done
+usage(){
+	printf "%s\n" "help"
+}
+
+while getopts ":e:n:s:c:dh" opt; do
+	case $opt in
+	    e)
+	    DM_VE="$OPTARG"
+#	    shift # past argument=value
+	    ;;
+	    n)
+		DM_NAME="$OPTARG"
+#	    shift # past argument=value
+		;;
+		s)
+		DM_SHARED_PATH="$OPTARG"
+#		shift
+		;;
+		host)                                                       
+	    DM_HOSTNAME="$OPTARG"                                                 
+#	    shift                                                                       
+	    ;;
+		c)                                                       
+	    DC_FILE="$OPTARG"                                                 
+#	    shift                                                                       
+	    ;;
+		d)
+		$DM_PATH rm -f $DM_NAME
+		exit 0
+#	    shift # past argument=value                                                 
+	    ;;
+		h)                                                                
+		usage
+#	    shift # past argument=value                                                 
+	    ;;
+		ls)                                                                  
+	    $DM_PATH ls
+		exit 0
+#	    shift # past argument=value                                                 
+	    ;;
+		ssh)
+	    $DM_PATH ssh $DM_NAME
+		exit 0
+#	    shift # past argument=value                                                 
+	    ;;
+		\?)
+		echo "Invalid option: -$OPTARG" >&2
+#		shift
+		;;
+		:)
+		echo "Option -$OPTARG requires an argument." >&2
+		exit 1
+		;;
+	    *)
+	    DM_SHARED_PATH=$PWD                                                             
+	    DM_NAME="dev"                                                                   
+	    DM_HOSTNAME="$(basename $DM_SHARED_PATH).local"                                 
+	    DC_FILE="$PWD/docker-compose.yml"                                               
+	            # unknown option
+	    ;;
+	esac
+done
+
 
 get_dm_tools
 start_dm_ve
